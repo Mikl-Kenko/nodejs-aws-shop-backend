7d68a04b7264fa903fec3c53162f93b9bb76c2c2
Mikl-Kenko <mikhail.kenko@gmail.com>
2023-06-20 13:32:27 +0300

    add import-service

---
+++ import-service/lambda/importProductsFile.ts
@@ -0,0 +1,34 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+export async function importProductsFile(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  try {
+    const fileName = event.queryStringParameters?.name;
+    console.log('importProductsFile input: ',JSON.stringify(event));
+    if (!fileName) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({errorMessage:'Please provide a file name.'}),
+      };
+    }
+    const s3 = new S3Client({ region: process.env.AWS_REGION });
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: `uploaded/${fileName}`,
+      Expires: 60,
+      ContentType: 'text/csv',
+    };
+    const command = new GetObjectCommand(params);
+    const url = await getSignedUrl(s3, command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(url),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Server Error' })
+    };
+  }
+}
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,62 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as s3Notifications from 'aws-cdk-lib/aws-s3-notifications';
+import * as path from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const shared: NodejsFunctionProps = {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      environment: {
+        S3_BUCKET_IMPORT_NAME : process.env.BUCKET_NAME|| '',
+        S3_BUCKET_IMPORT_REGION : process.env.AWS_REGION|| '',
+      },
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = s3.Bucket.fromBucketName(this, 'ImportedBucket', process.env.BUCKET_NAME || '');
+
+    const importProductsFile = new NodejsFunction(this, 'importProductsFile', {
+      ...shared,
+      functionName: 'importProductsFile',
+      entry: path.join(__dirname,'../lambda/importProductsFile.ts'),
+      handler: 'importProductsFile',
+    });
+
+    bucket.grantReadWrite(importProductsFile);
+
+    const importFileParser = new NodejsFunction(this, 'importFileParser', {
+      ...shared,
+      functionName: 'importFileParser',
+      entry: path.join(__dirname,'../lambda/importFileParser.ts'),
+      handler: 'importFileParser',
+    });
+
+    bucket.grantReadWrite(importFileParser);
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new s3Notifications.LambdaDestination(importFileParser), { prefix: 'uploaded' });
+
+    const api = new apiGateway.HttpApi(this, 'ImportApi', {
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowOrigins: ['*'],
+        allowMethods: [apiGateway.CorsHttpMethod.ANY],
+      },
+    });
+
+    api.addRoutes({
+      integration: new HttpLambdaIntegration('GetImportProductsFile', importProductsFile),
+      path: '/import',
+      methods: [apiGateway.HttpMethod.GET],
+    });
+
   }
 }
