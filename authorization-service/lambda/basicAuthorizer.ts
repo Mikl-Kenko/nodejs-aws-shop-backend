84f962090e28bde08073a8b96d5399aa40c837c2
Mikl-Kenko <mikhail.kenko@gmail.com>
2023-07-04 02:45:49 +0300

    feat: add auth CDK

---
+++ authorization-service/lambda/basicAuthorizer.ts
@@ -0,0 +1,50 @@
+import { APIGatewayTokenAuthorizerEvent, Callback, Context } from "aws-lambda";
+
+
+export const handler = async (
+  event: APIGatewayTokenAuthorizerEvent,
+  _ctx: Context,
+  cb: Callback
+) => {
+  console.log(_ctx);
+
+    try {
+      const { authorizationToken } = event;
+      const [, token] = authorizationToken.split(" ");
+        const [username, password] = Buffer.from(token, "base64")
+        .toString()
+        .split(":");
+  
+      const storedUserPassword = process.env[username];
+  
+      const effect =
+        !storedUserPassword || storedUserPassword !== password ? "Deny" : "Allow";
+  
+      const policy = generatePolicy(token, event.methodArn, effect);
+  
+      return cb(null, policy);
+    } catch (error: any) {
+      return cb(
+        `Unauthorized: ${
+          error instanceof Error ? error.message : "Unknown error!"
+        }`
+      );
+    }
+  };
+  
+  function generatePolicy(principalId: string, resource: string, effect: string) {
+    return {
+      principalId,
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: effect,
+            Resource: resource,
+          },
+        ],
+      },
+    };
+  }
+  
--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import path = require("path");
+import dotenv from 'dotenv';  
+dotenv.config();
+
+
+const AUTHORIZER_LAMBDA = "basicAuthorizer";
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const authorizerLambda = new NodejsFunction(this, AUTHORIZER_LAMBDA, {
+      functionName: AUTHORIZER_LAMBDA,
+      entry: path.join(__dirname, "..", "lambda", `${AUTHORIZER_LAMBDA}.ts`),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      environment: {
+        REGION: process.env.AWS_REGION,
+        AUTH_USER: process.env.AUTH_USER,
+        AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+      },
+    });
+    new cdk.CfnOutput(this, "ImportQueueArnOutput", {
+      value: authorizerLambda.functionArn,
+      exportName: "AuthorizerLambdaArn",
+    });
   }
 }
