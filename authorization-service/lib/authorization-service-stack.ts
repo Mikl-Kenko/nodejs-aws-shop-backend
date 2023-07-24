c1a61cbd8cd3609a92f98b01dd6a73e779e573ae
Mikl-Kenko <mikhail.kenko@gmail.com>
2023-07-24 22:55:42 +0300

    fix: stack

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,10 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import path = require("path");
-import dotenv from 'dotenv';  
+import * as dotenv from 'dotenv'; 
 dotenv.config();
 
 
@@ -19,9 +18,8 @@ export class AuthorizationServiceStack extends cdk.Stack {
       entry: path.join(__dirname, "..", "lambda", `${AUTHORIZER_LAMBDA}.ts`),
       runtime: lambda.Runtime.NODEJS_18_X,
       environment: {
-        REGION: process.env.AWS_REGION,
-        AUTH_USER: process.env.AUTH_USER,
-        AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+        AUTH_USER: process.env.AUTH_USER!,
+        AUTH_PASSWORD: process.env.AUTH_PASSWORD!,
       },
     });
     new cdk.CfnOutput(this, "ImportQueueArnOutput", {
